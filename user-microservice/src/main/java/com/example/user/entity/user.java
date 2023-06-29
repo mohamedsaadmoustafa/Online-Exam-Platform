8c7a3d70b50316f5b54a5451c16f9931114094f1
Mohamed S. Moustafa <mohamedsaadmoustafa@gmail.com>
2023-06-29 14:50:58 +0300

    solving firebase tokens problem

--- user-microservice/src/main/java/com/example/user/entity/user.java
+++ user-microservice/src/main/java/com/example/user/entity/user.java
@@ -1,4 +1,18 @@
 package com.example.user.entity;
 
-public class user {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Document(collection = "firebase_tokens")
+public class User {
+    String username;
+    String token;
 }
